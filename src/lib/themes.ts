2d093d8b329a6300fda4eda96d50eda958a52a03
Navya-re <134678539+Navya-re@users.noreply.github.com>
2023-08-31 23:06:42 +0530

    Add files via upload

---
+++ src/lib/themes.ts
@@ -0,0 +1,29 @@
+export enum InstanceClass {
+	man = 'Man',
+	woman = 'Woman',
+	youngmale = 'youngmale',
+	youngfemale = 'youngfemale',
+	cat = 'Cat'
+}
+
+export interface Theme {
+	name: string;
+	prompt: string;
+	instance_classes: InstanceClass[];
+}
+
+export const prompts: Theme[] = [
+	{
+		name: 'pixar character',
+		instance_classes: [],
+		prompt:
+			'realistic closeup portrait of @me as a jedi , highly detailed, science fiction, star wars concept art, intricate details, bright colors, golden hour, art by marko djurdjevic, greg rutkowski, wlop, fredperry, digital painting, rossdraws.'
+	}
+];
+
+export const getThemes = (instanceClass: string | null) =>
+	prompts.filter((item) => !item.instance_classes.includes(instanceClass as InstanceClass));
+
+export const getTheme = (theme: string) => prompts.find((item) => item.name == theme);
+
+export const getPrompt = (name: string) => prompts.find((prompt) => prompt.name == name)?.prompt;
