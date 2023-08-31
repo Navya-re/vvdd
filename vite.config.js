c4a13adcc13b0807d066568d853f7a78e1c42d78
Navya-re <134678539+Navya-re@users.noreply.github.com>
2023-09-01 03:15:30 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+	server: {
+		host: '0.0.0.0'
 	}
 };
 
